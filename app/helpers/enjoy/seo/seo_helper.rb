2f3aeee11f59171d298c7e458de508b93c1496da
Alexander Kiseliev <i43ack@gmail.com>
2016-05-25 16:00:59 +0300

    helper fix; GA and YM tags

---
+++ app/helpers/enjoy/seo/seo_helper.rb
@@ -0,0 +1,17 @@
+module Enjoy::Seo::SeoHelper
+
+  def enjoy_ym_counter_tag(counter_id)
+    render partial: "blocks/ym", locals: {counter_id: counter_id}
+  end
+  def enjoy_ga_counter_tag(counter_id)
+    render partial: "blocks/ga", locals: {counter_id: counter_id}
+  end
+  def render_enjoy_counters(opts = {})
+    ret = []
+    ym_counter_id = opts[:ym_counter_id] || Settings.ym_counter_id
+    ga_counter_id = opts[:ga_counter_id] || Settings.ga_counter_id
+    ret << enjoy_ym_counter_tag(ym_counter_id) unless ym_counter_id.blank?
+    ret << enjoy_ga_counter_tag(ga_counter_id) unless ga_counter_id.blank?
+    ret.join
+  end
+end
